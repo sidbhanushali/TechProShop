0f67f46edae01ab6656964de0ac4d1272a9d062c
sid <sid@sids-MacBook-Pro.local>
2020-10-04 17:15:34 -0700

    custom error middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,18 @@
+////Mongoose is rejecting the ID provided because it doesn't match the expected ObjectId type
+
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-
+//import middleware
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 //import routes
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
@@ -17,6 +18,10 @@ connectDB();
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
+
+//middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
