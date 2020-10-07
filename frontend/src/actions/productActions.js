44e4e9f475f405ecd4d1b4e84bf9e09fb4dfdb5d
sid <sid@sids-MacBook-Pro.local>
2020-10-07 12:21:29 -0700

    message and loader component and added to homeScreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,30 +1,28 @@
 import axios from "axios";
-
 import {
-  PRODUCT_LIST_FAIL,
-  PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
 } from "../constants/productConstants";
 
-//this action does what our useEffect does in the component
-//think of these functions as action creators
-
-//redux thunx (in our store) allows us to add a function after another function:
 export const listProducts = () => async (dispatch) => {
   try {
-    //set inital state with product list dispatch
+    //set inital state and axios req
     dispatch({ type: PRODUCT_LIST_REQUEST });
     //get productsList JSON from API
     const { data } = await axios.get("/api/products");
     //set productList state in the store to the data we got from API
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
   } catch (error) {
     //set payload to check for the generic error message and for the custom error message from errorHandler middleware
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload:
-        error.response && error.resonse.data.message
-          ? error.resonse.data.message
+        error.response && error.response.data.message
+          ? error.response.data.message
           : error.message,
     });
   }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+//redux imports
+import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
+//react-bootsrap import
+import { Row, Col } from "react-bootstrap";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -18,9 +22,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
