b800ffde69cc72b6ffdeb4b2bfaab75123d32156
sid <sid@sids-MacBook-Pro.local>
2020-10-20 14:39:02 -0700

    getOrderById controller & route to GET user's orderdetails

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 import { protectRoute } from "../middleware/authMiddleware.js";
+
+//TOP LEVEL ROUTE IS /api/orders
 
 //POST route for /api/orders --> create new order
 router.post("/", protectRoute, addOrderItems);
+
+//GET route for /api/orders/:id --> create new order
+router.get("/:id", protectRoute, getOrderById);
 
 export default router;
