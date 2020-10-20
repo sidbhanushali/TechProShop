50b662b408c6272b8fd25033f7e895db791b7400
sid <sid@sids-MacBook-Pro.local>
2020-10-20 10:35:22 -0700

    addOrderItems controller&/api//orders crate new order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
 const router = express.Router();
+import { addOrderItems } from "../controllers/orderController.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
 
-//top level route is /api/orders/
+//POST route for /api/orders --> create new order
+router.post("/", protectRoute, addOrderItems);
 
 export default router;
