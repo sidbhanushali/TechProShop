75a2764a646d78441c5c4012a14159014d37c830
sid <sid@sids-MacBook-Pro.local>
2020-10-20 21:59:04 -0700

    create orderScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
           <Route path='/placeorder' component={PlaceOrderScreen} exact />
           <Route path='/product/:id' component={ProductScreen} exact />
           <Route path='/cart/:id?' component={CartScreen} exact />
+          <Route path='/order/:id' component={OrderScreen} exact />
         </Container>
       </main>
 
