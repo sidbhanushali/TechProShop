ea69b3bef68317d30287af89f0b564f232fe3ee4
sid <sid@sids-MacBook-Pro.local>
2020-10-21 01:04:54 -0700

    OrderPay Constants,Actions,Reducer, init in store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,6 +19,7 @@ import {
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderPayReducer,
 } from "./reducers/orderReducers.js";
 
 const reducer = combineReducers({
@@ -31,6 +32,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
