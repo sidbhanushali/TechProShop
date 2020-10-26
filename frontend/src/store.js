8ff782a2321a9ade0a261fdf65efe3ad68436cad
sid <sid@sids-MacBook-Pro.local>
2020-10-25 18:30:43 -0700

    USER_UPDATE constants,reducer,actions, added update functionality on EditUserList.js page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,6 +16,7 @@ import {
   userUpdateProfileReducer,
   userListReducer,
   userDeleteReducer,
+  userUpdateReducer,
 } from "./reducers/userReducers.js";
 //IMPORT ORDER REDUCERS
 import {
@@ -35,6 +36,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
   userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
