034518a32f1188676446603f69be7f512a1eba62
sid <sid@sids-MacBook-Pro.local>
2020-10-27 23:37:52 -0700

    admin routes and controllers for createProduct and updateProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,7 +8,7 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
-//controller for public route  GET /api/products -- returns single product by _objectID
+//controller for public route  GET /api/products -- gets single product by _objectID
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
@@ -34,4 +34,60 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+// Controller for  POST /api/products -->  admin Create a product
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// Controller for PUT /api/products/:id --> admin update a product
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  //replace the current product info with the form data comin from req.body
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,6 +4,8 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 
 import { protectRoute, checkAdmin } from "../middleware/authMiddleware.js";
@@ -13,7 +15,13 @@ router.get("/", getProducts);
 // @route   GET /api/products/:id
 router.get("/:id", getProductById);
 
-// @route  DELETE /api/products/:id
+// @route  DELETE /api/products/:id -- ADMIN ROUTE
 router.delete("/:id", protectRoute, checkAdmin, deleteProduct);
+
+//@route POST /api/products --> only admins can create products
+router.post("/", protectRoute, checkAdmin, createProduct);
+
+//@route PUT /api/products/:id --> only admins can update products
+router.put("/:id", protectRoute, checkAdmin, updateProduct);
 
 export default router;
