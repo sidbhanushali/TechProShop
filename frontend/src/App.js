603ea4eaeaf2103c4d59453cc80155bc11dfa710
sid <sbhanushali415@gmail.com>
2020-10-28 01:08:17 -0700

    ProductEditScreen jsx and reactrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 const App = () => {
   return (
@@ -36,8 +37,9 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} exact />
           <Route path='/order/:id' component={OrderScreen} exact />
           <Route path='/admin/userlist' component={UserListScreen} exact />
-          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} exact />
           <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
         </Container>
       </main>
 
