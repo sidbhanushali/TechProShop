81978437ddd83fe0b47d6450261cd78eac33102d
sid <sbhanushali415@gmail.com>
2020-10-27 23:49:46 -0700

    createProduct and updateProduct edit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -21,7 +21,7 @@ router.delete("/:id", protectRoute, checkAdmin, deleteProduct);
 //@route POST /api/products --> only admins can create products
 router.post("/", protectRoute, checkAdmin, createProduct);
 
-//@route PUT /api/products/:id --> only admins can update products
+//@route PUT /api/products/:id -->only admins can update products
 router.put("/:id", protectRoute, checkAdmin, updateProduct);
 
 export default router;
