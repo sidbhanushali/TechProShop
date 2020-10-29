56b5b446e2cdddf1726926a392895a41b14d4ba1
sid <sbhanushali415@gmail.com>
2020-10-29 13:04:50 -0700

    image upload backend  multer config and uploadRoute.js

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,50 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    //add uploads file from root dir
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(
+      //null for the error
+      null, //change uploaded file name to filename-DATENOW().fileExt
+      //node.js path module to get the extension of the file
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+//create a custom function to check the file type (filter for .png upload files only
+function checkFileType(file, cb) {
+  //test the possible file types against the filetype of the upload object
+  const filetypes = /jpg|jpeg|png/;
+  //.test()returns BOOL
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // mimetype is where .jpeg = img/jpeg
+  //.test()returns BOOL
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Images only!");
+  }
+}
+
+//this is what gets passed into our route
+const upload = multer({
+  //pass in the created storage object and create a custom function to check the file type (filter for .png upload files only)
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
@@ -8,6 +9,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -20,10 +22,17 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/uploads", uploadRoutes);
 //PAYPAL CLIENT ROUTE
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+//static uploads folder
+//__dirname not available if using ES6 imports with node.js
+//creating a variable that will point to the same thing
+const dirname_sub = path.resolve();
+app.use("/uploads", express.static(path.join(dirname_sub, "/uploads")));
 
 //middleware
 app.use(notFound);
