4909ee6e8b78382890d06df80be3e551eb268741
sid <sbhanushali415@gmail.com>
2020-11-04 01:01:10 -0800

    fix order of productRoutes to load carousel on admin homepage

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,6 +14,9 @@ import { protectRoute, checkAdmin } from "../middleware/authMiddleware.js";
 // @route   GET /api/products
 router.get("/", getProducts);
 
+//route for POST /api/products/:id/reviews --> THIS NEEDS TO BE LOADED FIRST BC IT IS A DEPENDENCY FOR THE PRODUCT CAROUSEL WHICH NEEDS THE REVEIWS TO LOAD PROPERLY
+router.post("/:id/reviews", protectRoute, createProductReview);
+
 //@route GET /api/products/top --> returns top 3 products sorted by reviews
 router.get("/top", getTopProducts);
 
@@ -28,8 +31,5 @@ router.post("/", protectRoute, checkAdmin, createProduct);
 
 //@route PUT /api/products/:id -->only admins can update products
 router.put("/:id", protectRoute, checkAdmin, updateProduct);
-
-//route for POST /api/products/:id/reviews
-router.post("/:id/reviews", protectRoute, createProductReview);
 
 export default router;
