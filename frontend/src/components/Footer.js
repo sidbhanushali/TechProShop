674a37d1ece2c8d8e5a3bb0ce6ede730786ac426
sid <sbhanushali415@gmail.com>
2020-11-04 01:07:14 -0800

    add GH link to Footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,13 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>
-            Copyright &copy; Siddharth Bhanushali
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.github.com/sidbhanushali'
+            >
+              Copyright &copy; Siddharth Bhanushali
+            </a>
           </Col>
         </Row>
       </Container>
