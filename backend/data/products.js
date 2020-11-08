bdf2b9ea23ebf2613e56024ce7720a461f83cee0
sid <sbhanushali415@gmail.com>
2020-11-08 01:57:47 -0800

    added products for seeder script

--- backend/data/products.js
+++ backend/data/products.js
@@ -71,6 +71,57 @@ const products = [
     rating: 4,
     numReviews: 12,
   },
+  {
+    name: "DJI Mavic 2 Pro - Drone Quadcopter UAV",
+    image: "/images/drone.jpg",
+    description:
+      'DJI Mavic 2 Pro Drone Quadcopter with Hasselblad Camera HDR Video UAV Adjustable Aperture 20MP 1" CMOS Sensor (US Version). Advanced Pilot Assistance Systems (APAS) allows the aircraft to actively avoid obstacles in front of and behind it.',
+    brand: "DJI",
+    category: "Electronics",
+    price: 1599.99,
+    countInStock: 2,
+    rating: 5,
+    numReviews: 0,
+  },
+
+  {
+    name: "LG 34inch Ultragear Monitor ",
+    image: "/images/lgmonitor.jpeg",
+    description:
+      "Delivers the latest hardware, specs, ergonomics, sleek design and sensory experience. With gaming-focused features like NVIDIA G-SYNC compatibility, 1ms GTG response times, pro-level customization and fast, vivid IPS panels, no wonder it's #1 gaming-designed monitor brand in U.S",
+    brand: "LG",
+    category: "Electronics",
+    price: 999.99,
+    countInStock: 2,
+    rating: 5,
+    numReviews: 0,
+  },
+
+  {
+    name: "OnePlus 8T Aquamarine Green, 5G ",
+    image: "/images/oneplus8t.jpg",
+    description:
+      "5G Unlocked Android Smartphone U.S Version, 12GB RAM+256GB Storage, 120Hz Fluid Display,Quad Camera, 65W Ultra Fast Charge, Lunar Silver, U.S Warranty. T-Mobile version available through carrier.",
+    brand: "OnePlus",
+    category: "Electronics",
+    price: 749.99,
+    countInStock: 2,
+    rating: 5,
+    numReviews: 0,
+  },
+
+  {
+    name: "2019 Apple MacBook Pro 13.3inch",
+    image: "/images/macbookpro.jpg",
+    description:
+      "With the 8th Generation Intel Core processors, MacBook Pro reaches new heights in compute performance. The 8th Generation quad‑core processor on the 13‑inch MacBook Pro with Touch Bar makes it ready to take on even the toughest tasks.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 1399.99,
+    countInStock: 2,
+    rating: 5,
+    numReviews: 0,
+  },
 ];
 
 export default products;
